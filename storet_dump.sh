6e9c6515f953fadcf726bf88b916b556df436cbb
Dave Steinich <drsteini@usgs.gov>
2013-09-10 14:54:55 +0000

    testing new scripts on eros

---
+++ storet_dump.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export work=/opt/tomcat/wqp/storet
+# export work=/b_test_dir/oradata/storet_20110822
+export file_stub=stormodb_shire_storetw
+export explog=${file_stub}_expdp.log
+export expref=${file_stub}_expdp.ref
+export http_base=http://www.epa.gov/storet/download/storetw
+export date_suffix=`date +%Y%m%d_%H%M`
+
+cd $work
+
+(
+
+curl $http_base/$explog > $explog 2> curlout.log.1
+
+egrep '^Export|successfully completed' $explog
+export table_count=`grep "exported " $explog | wc -l`
+export complete_count=`grep "successfully completed" $explog | wc -l`
+
+if [ $table_count -ne 14 -o $complete_count -ne 1 ] ; then
+   echo "table_count not 14 or complete_count not 1 - "$table_count" "$complete_count". quitting."
+   exit 1
+fi
+
+if [ -f $expref ] ; then
+   diffs=`diff $explog $expref | wc -l`
+   echo $diffs " differences found between log file and base ref"
+   if [ $diffs -eq 0 ] ; then
+      echo "Since no differences, quitting."
+      exit 1
+   fi
+else
+   echo "No reference for comparison."
+fi
+
+
+count=0
+
+for i  in `grep orabackup $explog` ; do
+   count=`expr $count + 1`
+   basename=`basename $i`
+   tries=1
+   while [ $tries -le 3 ] ; do
+      date
+      echo "downloading "$basename" ...file "$count"   attempt "$tries
+      curl $http_base/$basename > $basename 2> curlout.$count.$tries
+      status=$?
+      echo "status: "$status
+      if [ $status -eq 0 ] ; then
+         break
+      else
+         sleep 300
+      fi
+      tries=`expr $tries + 1`
+   done
+   if [ $tries -gt 3 ] ; then
+      echo "failed to download "$basename"   quitting."
+      exit 1
+   fi
+done
+
+if [ $count -eq 0 ] ; then
+   echo "no files found.  quitting."
+   exit 1
+fi
+
+echo "downloaded "$count" files"
+
+exit 0 
