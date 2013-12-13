6e850a9d23e945e4f6697b8c37a80393d8d4d63b
Dave Steinich <drsteini@usgs.gov>
2013-12-13 17:27:28 +0000

    try running package a different way

---
+++ runPackageStage.sql
@@ -0,0 +1,16 @@
+show user;
+select * from global_name;
+whenever sqlerror exit failure rollback;
+whenever oserror exit failure rollback;
+set timing on;
+set serveroutput on;
+set linesize 160
+exec dbms_output.enable(100000);
+select 'start time: ' || systimestamp from dual;
+
+begin
+   create_storet_objects.main;
+end;
+/
+
+select 'end time: ' || systimestamp from dual;
