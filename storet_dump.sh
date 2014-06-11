c85a768853aaad9f0d053f959bcf637f823c3470
Dave Steinich <drsteini@usgs.gov>
2014-06-11 17:51:38 +0000

    They are now sending 28 tables

--- storet_dump.sh
+++ storet_dump.sh
@@ -17,8 +17,8 @@ egrep '^Export|successfully completed' $explog
 export table_count=`grep "exported " $explog | wc -l`
 export complete_count=`grep "successfully completed" $explog | wc -l`
 
-if [ $table_count -ne 23 -o $complete_count -ne 1 ] ; then
-   echo "table_count("$table_count") not 19 or complete_count("$complete_count") not 1. quitting."
+if [ $table_count -ne 28 -o $complete_count -ne 1 ] ; then
+   echo "table_count("$table_count") not 28 or complete_count("$complete_count") not 1. quitting."
    exit 1
 fi
 
