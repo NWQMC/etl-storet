578eeca3f02d03cb2acf7b53164bfe8fad0d6b74
Dave Steinich <drsteini@usgs.gov>
2014-06-27 17:27:11 +0000

    hardening of script - WQP-462

--- storet_dump.sh
+++ storet_dump.sh
@@ -17,8 +17,8 @@ egrep '^Export|successfully completed' $explog
 export table_count=`grep "exported " $explog | wc -l`
 export complete_count=`grep "successfully completed" $explog | wc -l`
 
-if [ $table_count -ne 28 -o $complete_count -ne 1 ] ; then
-   echo "table_count("$table_count") not 28 or complete_count("$complete_count") not 1. quitting."
+if [ $table_count -ne $exp_table_count -o $complete_count -ne 1 ] ; then
+   echo "table_count("$table_count") not $exp_table_count or complete_count("$complete_count") not 1. quitting."
    exit 1
 fi
 
@@ -26,7 +26,7 @@ if [ -f $expref ] ; then
    diffs=`diff $explog $expref | wc -l`
    echo $diffs " differences found between log file and base ref"
    if [ $diffs -eq 0 ] ; then
-      echo "Since no differences, quitting."
+      echo "Since no differences, we are done."
       exit 1
    fi
 else
@@ -34,40 +34,8 @@ else
 fi
 
 
-count=0
-
-for i  in `grep orabackup $explog` ; do
-   count=`expr $count + 1`
-   basename=`basename $i`
-   tries=1
-   while [ $tries -le 3 ] ; do
-      date
-      echo "downloading "$basename" ...file "$count"   attempt "$tries
-      wget $http_base/$basename
-      status=$?
-      echo "status: "$status
-      if [ $status -eq 0 ] ; then
-         break
-      else
-         sleep 300
-      fi
-      tries=`expr $tries + 1`
-   done
-   if [ $tries -gt 3 ] ; then
-      echo "failed to download "$basename"   quitting."
-      exit 1
-   fi
-done
-
-if [ $count -eq 0 ] ; then
-   echo "no files found.  quitting."
-   exit 1
-fi
-
-echo "downloaded "$count" files"
+files=`grep orabackup test.log | sed -e 's/^.*\//http:\/\/www.epa.gov\/storet\/download\/storetw\//'`
+echo $files
+echo $files | xargs -n 1 -P 8 wget
 
 ) 2>&1 | tee storet_dump_$date_suffix.out
-
-echo "got here"
-
-exit 0 
