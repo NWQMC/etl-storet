b641781c6c08ca43b438742a2dedd2a4f9a1405e
abramhall <abramhall@usgs.gov>
2014-07-21 08:06:10 -0500

    fail faster

--- create_storet_objects_stage.sql
+++ create_storet_objects_stage.sql
@@ -753,8 +753,8 @@ create or replace package body create_storet_objects
       dbms_output.enable(100000);
 
       dbms_output.put_line(systimestamp || ' started storet table transformation.');
-      create_regular_result;
       create_station;
+      create_regular_result;
       create_biological_result_temp;
       create_lookups;
       create_summaries;
